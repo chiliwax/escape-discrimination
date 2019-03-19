da43648103599891357e4c1d4f831685922bf278
chiliwa <llecointe.thibault@gmail.com>
2019-03-19 15:02:21 +0100

    Add Splash Screen

---
+++ EscapeDiscrimination/Assets/Scripts/press_any_key.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class press_any_key : MonoBehaviour
+{
+    public string _scene;
+    void Update()
+    {
+        if (Input.anyKey)
+            SceneManager.LoadScene(_scene);
+    }
+}
---
+++ EscapeDiscrimination/Assets/Scripts/text_blinking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class text_blinking : MonoBehaviour
+{
+    private bool flop = false;
+    public float speed = 0;
+
+    void Start()
+    {
+        StartCoroutine(Text_blinking());
+    }
+
+    IEnumerator Text_blinking()
+    {
+
+        Color color;
+        TextMeshProUGUI mText = gameObject.GetComponent<TextMeshProUGUI>();
+
+        while (true)
+        {
+            color = mText.color;
+            if (flop == true)
+            {
+                color.a += 0.1f;
+                if (color.a >= 1)
+                {
+                    flop = false;
+                }
+            }
+            else
+            {
+                color.a -= 0.1f;
+                if (color.a <= 0)
+                {
+                    flop = true;
+                }
+            }
+            mText.color = color;
+            yield return new WaitForSeconds(speed);
+        }
+    }
+
+
+}
