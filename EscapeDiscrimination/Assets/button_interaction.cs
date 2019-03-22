e0e1ef4898add787402c902c3506c3bbc0e28fd1
chiliwa <llecointe.thibault@gmail.com>
2019-03-22 14:14:44 +0100

    level1

---
+++ EscapeDiscrimination/Assets/button_interaction.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button_interaction : MonoBehaviour
+{
+    public Sprite activate_button;
+    public UnityEngine.Events.UnityEvent button_trigger;
+ void OnTriggerEnter2D(Collider2D col)
+    {
+        if (LayerMask.LayerToName(col.gameObject.layer) == "Player")
+        {
+            GetComponent<SpriteRenderer>().sprite = activate_button;
+            button_trigger.Invoke();
+        }
+    }
+}
